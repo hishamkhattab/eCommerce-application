bf234e7b22ecaa8053a4521bdd150166ff4f8148
hishamkhattab <khattab648@gmail.com>
2022-07-05 22:31:02 +0200

    Create footer

---
+++ src/components/footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import "./style.scss";
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <div className="footer-wrapper">
+                <h1>FOOTER</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
